64f804a04093db788b02c701974888d0ffa9e7fa
chu2bard <bxndnbdnd29@gmail.com>
2026-02-04 16:21:40 +0100

    add executor

--- execbox/policy.py
+++ execbox/policy.py
@@ -86,6 +86,3 @@ class PolicyChecker:
         if not self._import_allowed(module):
             raise PolicyViolation(f"import not allowed: {name}")
         return __builtins__["__import__"](name, *args, **kwargs)
-
-
-
