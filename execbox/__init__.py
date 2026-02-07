b4a209c22b9e99af37e9c6b9e1ff19febc5f0767
chu2bard <bxndnbdnd29@gmail.com>
2026-02-07 14:26:45 +0100

    add server support

--- execbox/__init__.py
+++ execbox/__init__.py
@@ -1,9 +1,19 @@
-# note: revisit later
-# cleanup: improve this
 """execbox - code execution sandbox for AI agents."""
-# note: edge case
 
 __version__ = "0.1.0"
 
+from execbox.types import ExecResult, ExecStatus, ResourceLimits, PolicyConfig
+from execbox.sandbox import Sandbox
+from execbox.executor import Executor
+from execbox.policy import PolicyChecker, PolicyViolation
 
-
+__all__ = [
+    "Sandbox",
+    "Executor",
+    "ExecResult",
+    "ExecStatus",
+    "ResourceLimits",
+    "PolicyConfig",
+    "PolicyChecker",
+    "PolicyViolation",
+]
